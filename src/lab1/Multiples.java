d44bec83022306b288604d577318e4e2d141fa5e
Anna Oh <anna@Annas-MacBook-Air-4.local>
2025-09-15 02:29:53 -0400

    update Multiples.java

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,13 +2,20 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+
+    }
+
+    public static int multiples(int n, int a, int b) {
         int i = 1;
-        while (i <= 1000) {
-            if (i % 3 == 0 || i % 5 == 0) {
+        int count = 0;
+        while (i < n) {
+            if (i % a == 0 || i % b == 0) {
                 System.out.println(i);
+                count++;
             }
             i++;
         }
-
+        return count;
     }
 }
